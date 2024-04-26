0945b5f33bb5f7d02ad30983410ca56aa2db2ac9
cumic <changhwan060224@gmail.com>
2024-04-26 23:55:00 +0900

    WeightRandom
    가중치 랜덤 만드는중

--- Assets/C#/WeightRandom.cs
+++ Assets/C#/WeightRandom.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class WeightRandom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private List<int> weightList = new();
 
-    // Update is called once per frame
-    void Update()
+    [ContextMenu("Random")]
+    public void GetRandomValue()
     {
-        
+        int maxWeight = 0;
+
+        foreach (var item in weightList)
+        {
+            maxWeight += item;
+        }
+
+        int randomValue = Random.Range(0, maxWeight);
+
+        for (int i = 0; i < weightList.Count; i++)
+        {
+            if (randomValue >= weightList[i])
+            {
+                Debug.Log(weightList[i].ToString());
+                return;
+            }
+        }
     }
 }
